6f7ba463c646148c9fb64308f4fe09e858757b8a
Tarunkumar02 <155305197+Tarunkumar02@users.noreply.github.com>
2026-01-11 23:56:25 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://chat-based-personal-fitness-trainer.onrender.com',
   headers: {
     'Content-Type': 'application/json'
   }
